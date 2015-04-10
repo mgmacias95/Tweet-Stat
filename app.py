753fae91d71db4c3d23245e89de9c3b757b1984e
mgmacias95 <mgmacias95@correo.ugr.es>
2015-04-10 22:49:20 +0200

    filtra rts de los ultimos 1000 tweets del usuario

--- app.py
+++ app.py
@@ -24,11 +24,22 @@ def retweets_of():
     #Get user time line
     my_timeline = api.user_timeline(user.id, exclude_replies=True, counter=200)
     my_rts = rt_filtrer(my_timeline)
+    my_last_tweet = my_timeline[-1].id
     rted_people = []
 
-    # get the author of each retweet
-    for rt in my_rts:
-        rted_people.append(rt.retweeted_status.user.screen_name)
+    for i in range(200,1000,200):
+        # get the author of each retweet
+        for rt in my_rts:
+            rted_people.append(rt.retweeted_status.user.screen_name)
+
+        my_timeline = []
+        my_rts = []
+        my_timeline = api.user_timeline(user.id, exclude_replies=True, counter=200, max_id=my_last_tweet)
+        my_last_tweet = my_timeline[-1].id
+        my_rts = rt_filtrer(my_timeline)
+    
+    for rt in rted_people:
+        print(rt)
 
 
 if __name__ == '__main__':
