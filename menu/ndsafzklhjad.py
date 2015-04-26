b30d857218e902524b90139a7e2121f9c2c79340
BraulioV <brauliovarlop@correo.ugr.es>
2015-04-26 20:42:57 +0200

    clase menu terminada

--- menu/ndsafzklhjad.py
+++ menu/ndsafzklhjad.py
@@ -136,7 +136,7 @@ class Menu:
             print("Oups! Something is wrong with your choice.")
 
             while self.suboption_menu < -1 or self.suboption_menu > num_opts:
-                self.suboption_menu = int(input("Try to choose a valid option: "))
+                self.option = int(input("Try to choose a valid option: "))
 
 if __name__ == '__main__':
     menu = Menu()
@@ -155,103 +155,28 @@ if __name__ == '__main__':
                 if menu.suboption_menu == 0:
                     print("Comming soon...")
                     menu.print_user_menu()
-                    menu.set_submenu_opt(num_opt-1)
+                    menu.set_submenu_opt()
                 elif menu.suboption_menu == 1:
                     print("Comming soon...")
                     menu.print_user_menu()
-                    menu.set_submenu_opt(num_opt-1)
+                    menu.set_submenu_opt()
 
             menu.print_main_menu()
             menu.set_main_menu_opt()
 
         elif menu.option == 1:
-            num_opt = 4
             menu.print_rt_menu()
-            menu.set_submenu_opt(num_opt-1)
-            while menu.suboption_menu != -1:
-                if menu.suboption_menu == 0:
-                    print("Calling more rted users...")
-                    menu.print_rt_menu()
-                    menu.set_submenu_opt(num_opt-1)
-
-                elif menu.suboption_menu == 1:
-                    print("Calling user that make mor rt\'s")
-                    menu.print_rt_menu()
-                    menu.set_submenu_opt(num_opt-1)
-
-                elif menu.suboption_menu == 2:
-                    print("Top 10 rted\'s tweets")
-                    menu.print_rt_menu()
-                    menu.set_submenu_opt(num_opt-1)
-
-                elif menu.suboption_menu == 3:
-                    print("Average rt per day")
-                    menu.print_rt_menu()
-                    menu.set_submenu_opt(num_opt-1)
-
-            menu.print_main_menu()
-            menu.set_main_menu_opt()
+            menu.suboption_menu = int(input("Choose a valid submenu option: "))
 
         elif menu.option == 2:
-            num_opt = 4
             menu.print_fav_menu()
-            menu.set_submenu_opt(num_opt-1)
-            
-            while menu.suboption_menu != -1:
-                if menu.suboption_menu == 0:
-                    print("More fav users...")
-                    menu.print_fav_menu()
-                    menu.set_submenu_opt(num_opt-1)
-
-                elif menu.suboption_menu == 1:
-                    print("User that make favorite you more")
-                    menu.print_fav_menu()
-                    menu.set_submenu_opt(num_opt-1)
-
-                elif menu.suboption_menu == 2:
-                    print("Top 10 fav\'s tweets")
-                    menu.print_fav_menu()
-                    menu.set_submenu_opt(num_opt-1)
-
-                elif menu.suboption_menu == 3:
-                    print("Average fav per day")
-                    menu.print_fav_menu()
-                    menu.set_submenu_opt(num_opt-1)
-
-            menu.print_main_menu()
-            menu.set_main_menu_opt()
-
+            menu.suboption_menu = int(input("Choose a valid submenu option: "))
         elif menu.option == 3:
-            num_opt = 3
             menu.print_follower_menu()
-            menu.set_submenu_opt(num_opt-1)
-            
-            while menu.suboption_menu != -1:
-                if menu.suboption_menu == 0:
-                    print("Not follow back")
-                    menu.print_follower_menu()
-                    menu.set_submenu_opt(num_opt-1)
-
-                elif menu.suboption_menu == 1:
-                    print("New follower\'s")
-                    menu.print_follower_menu()
-                    menu.set_submenu_opt(num_opt-1)
-
-                elif menu.suboption_menu == 2:
-                    print("Last unfollows")
-                    menu.print_follower_menu()
-                    menu.set_submenu_opt(num_opt-1)
-
-            menu.print_main_menu()
-            menu.set_main_menu_opt()
-
+            menu.suboption_menu = int(input("Choose a valid submenu option: "))
         elif menu.option == 4:
-            num_opt = 1
             menu.extra()
-            menu.set_submenu_opt(num_opt-1)
-            while menu.suboption_menu != -1:
-                menu.set_submenu_opt(num_opt-1)
-
+            menu.set_submenu_opt()
             menu.print_main_menu()
             menu.set_main_menu_opt()
 
